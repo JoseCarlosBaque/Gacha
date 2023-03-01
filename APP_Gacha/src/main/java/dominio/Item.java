a6222112e413be267cdd1b5842e4b84cb1306c6d
Jose Carlos Baque Meneses <josebaque03@gmail.com>
2023-03-01 13:31:49 +0100

    clases carta e item

---
+++ APP_Gacha/src/main/java/dominio/Item.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author joseb
+ */
+public class Item {
+    private int id_item;
+    private String nombre;
+}
